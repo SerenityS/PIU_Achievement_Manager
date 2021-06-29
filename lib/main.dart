95b4be69da895e400d32933caec45a6ad7601337
HeeRyun, Jin <jins4218@gmail.com>
2021-06-29 11:27:56 +0900

    Small Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:piu_achievement_manager/screens/SongListScreen.dart';
+import 'package:piu_achievement_manager/screens/song_list_screen.dart';
 
 void main() async {
   await GetStorage.init();
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
       initialRoute: '/songlist',
-      getPages: [GetPage(name: '/songlist', page: () => SongListScreen())],
+      getPages: [GetPage(name: '/songlist', page: () => SongListScreen(),)],
     );
   }
 }
